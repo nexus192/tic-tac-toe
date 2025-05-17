4af195bae1ee3c5b1aa157e78bc2c16cc660630e
nexus192 <88116739+nexus192@users.noreply.github.com>
2025-05-17 14:46:03 +0300

    rewrite controller in class

--- src/web/controller.py
+++ src/web/controller.py
@@ -7,63 +7,73 @@ from uuid import UUID
 
 app = Flask(__name__, template_folder='templates')
 
-# game = WebGame()
-di_game = Container()
-web_mapper = WebMapper()
-source_mapper = SourceMapper()
-games_by_id: dict[str, WebGame] = {}
 
+class Conroller:
 
-@app.route('/game/<game_id>', methods=['GET', 'POST'], endpoint='game')
-def game_la(game_id):
-  game_id = UUID(game_id)
+  def __init__(self, app: Flask, di_game: Container, web_mapper: WebMapper,
+               source_mapper: SourceMapper):
+    self.app: Flask = app
+    self.DI: Container = di_game
+    self.web_mapper: WebMapper = web_mapper
+    self.source_mapper: SourceMapper = source_mapper
 
-  game = web_mapper.from_domain_to_web(
-      source_mapper.from_storage_to_domain(
-          di_game.game_data_service.get_current_game(game_id)))
+    self.register_routes()
 
-  if request.method == 'POST':
-    row = int(request.form['row'])
-    col = int(request.form['col'])
-    if game.game_board.cells[row][col] == 0:
-      web_mapper.from_web_to_domain(game).make_move(row, col,
-                                                    game.game_board.PLAYER_X)
-      ai_move = di_game.domain.get_next_move(
-          web_mapper.from_web_to_domain(game))
-      if ai_move is not None:
-        row_ai, col_ai = ai_move
-        web_mapper.from_web_to_domain(game).make_move(row_ai, col_ai,
-                                                      game.game_board.PLAYER_O)
+  def register_routes(self):
+    self.app.add_url_rule('/', view_func=self.start_window)
+    self.app.add_url_rule('/new_game', view_func=self.new_game)
+    self.app.add_url_rule('/all_games', view_func=self.all_games)
+    self.app.add_url_rule('/game/<game_id>',
+                          view_func=self.game_view,
+                          methods=['POST', 'GET'],
+                          endpoint='game')
 
-  return render_template('board.html',
-                         board=game.game_board.cells,
-                         game_id=game.game_id)
+  def game_view(self, game_id):
+    game_id = UUID(game_id)
 
+    game = self.web_mapper.from_domain_to_web(
+        self.source_mapper.from_storage_to_domain(
+            self.DI.game_data_service.get_current_game(game_id)))
 
-@app.route('/')
-def start_game():
-  return render_template('navigation_bar.html')
+    if request.method == 'POST':
+      row = int(request.form['row'])
+      col = int(request.form['col'])
+      if game.game_board.cells[row][col] == 0:
+        self.web_mapper.from_web_to_domain(game).make_move(
+            row, col, game.game_board.PLAYER_X)
+        ai_move = self.DI.domain.get_next_move(
+            self.web_mapper.from_web_to_domain(game))
+        if ai_move is not None:
+          row_ai, col_ai = ai_move
+          self.web_mapper.from_web_to_domain(game).make_move(
+              row_ai, col_ai, game.game_board.PLAYER_O)
 
+    return render_template('board.html',
+                           board=game.game_board.cells,
+                           game_id=game.game_id)
 
-@app.route("/new_game")
-def new_game():
-  game = WebGame()
-  di_game.game_data_service.save_current_game(
-      source_mapper.from_domain_to_storage(web_mapper.from_web_to_domain(game)))
-  return redirect(url_for('game', game_id=game.game_id))
+  def start_window(self):
+    return render_template('navigation_bar.html')
 
+  def new_game(self):
+    game = WebGame()
+    self.DI.game_data_service.save_current_game(
+        self.source_mapper.from_domain_to_storage(
+            self.web_mapper.from_web_to_domain(game)))
+    return redirect(url_for('game', game_id=game.game_id))
 
-@app.route("/all_game")
-def all_game():
-  games = di_game.game_data_service.get_all_games()
-  return render_template('all_games.html', games=games.keys())
+  def all_games(self):
+    games = self.DI.game_data_service.get_all_games()
+    return render_template('all_games.html', games=games.keys())
 
-
-def get_local_ip():
-  import socket
-  return socket.gethostbyname(socket.gethostname())
+  @staticmethod
+  def get_local_ip():
+    import socket
+    return socket.gethostbyname(socket.gethostname())
 
 
 if __name__ == "__main__":
+
+  controller = Conroller(app, Container(), WebMapper(), SourceMapper())
   app.run(host='0.0.0.0', port=5000, debug=True)
   print(f"Сервер доступен по адресу: http://{get_local_ip()}:5000")
